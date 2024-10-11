fc3d1a8abe910fd7deed2597424f00adaa811c00
Maaz Shaikh <maaz.z.shaikh@gmail.com>
2024-10-11 17:29:57 +0530

    Time: 1 ms (99.8%), Space: 46.3 MB (25.64%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-       Map<Integer,Integer> mpp = new HashMap<>();
-       for(int i =0;i<nums.length;i++){
-            int val = mpp.getOrDefault(nums[i],0);
-            mpp.put(nums[i],val+1);
-       }
-
-       for(Map.Entry<Integer,Integer> it:mpp.entrySet()){
-        if(it.getValue()==1) return it.getKey();
-       }
-       return -1;
+        int ans =0;
+        for(int i =0;i<nums.length;i++){
+            ans= ans^nums[i];
+        }
+        return ans;
     }
 }
